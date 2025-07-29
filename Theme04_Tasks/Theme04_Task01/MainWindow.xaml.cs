6cff6e45c8b39223ce88113fd6b16b5d709c3216
Ilya Par <ilya-par@yandex.ru>
2025-07-29 22:42:13 +0300

    Окончательное решение задачи по Теме 4.

--- Theme04_Tasks/Theme04_Task01/MainWindow.xaml.cs
+++ Theme04_Tasks/Theme04_Task01/MainWindow.xaml.cs
@@ -14,53 +14,37 @@ namespace Theme04_Task01
     public class ToggleButton : Button
     {
         public static readonly DependencyProperty IsToggledProperty =
-            DependencyProperty.Register(
-                "IsToggled", // Имя свойства
-                typeof(bool), // Тип свойства
-                typeof(ToggleButton), // Тип класса, где определено свойство
-                new PropertyMetadata(false, OnIsToggledChanged)); // Значение по умолчанию и обработчик изменения
-
-        // Обертка над DependencyProperty для удобного использования
+            DependencyProperty.Register
+            (
+                "IsToggled",
+                typeof(bool),
+                typeof(ToggleButton),
+                new PropertyMetadata(false, OnIsToggledChanged)
+            );
         public bool IsToggled
         {
-            get { return (bool)GetValue(IsToggledProperty); }
-            set { SetValue(IsToggledProperty, value); }
+            get => (bool)GetValue(IsToggledProperty);
+            set => SetValue(IsToggledProperty, value);
         }
-
-        // Обработчик изменения значения IsToggled
         private static void OnIsToggledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var button = d as ToggleButton;
-            if (button != null)
-            {
-                // Изменяем фон и текст в зависимости от состояния
-                button.UpdateAppearance();
-            }
-        }
 
-        // Конструктор
+            if (button != null)
+                button.UpdateAppearance();
+        }
         public ToggleButton()
         {
-            // Устанавливаем начальное значение текста и фона
             UpdateAppearance();
-            // Добавляем обработчик щелчка мыши
             Click += OnClick;
         }
-
-        // Обработчик щелчка мыши
         private void OnClick(object sender, RoutedEventArgs e)
         {
-            // Переключаем состояние IsToggled
             IsToggled = !IsToggled;
         }
-
-        // Метод для обновления внешнего вида кнопки
         private void UpdateAppearance()
         {
-            // Изменяем текст
             Content = IsToggled ? "ON" : "OFF";
-
-            // Изменяем цвет фона
             Background = IsToggled ? Brushes.Green : Brushes.Red;
         }
     }
