d8abfc8f2ff0b38e420b463ff77cff9027b15bf7
Ilya Par <ilya-par@yandex.ru>
2025-08-01 20:58:21 +0300

    Решение задачи.

---
+++ Theme06_Tasks/Theme06_Task01/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Theme06_Task01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
