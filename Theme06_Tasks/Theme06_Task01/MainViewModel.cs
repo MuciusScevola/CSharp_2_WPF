45b04e4063302e88de3152b6feb932105c53170d
Ilya Par <ilya-par@yandex.ru>
2025-08-13 22:18:58 +0300

    Решение задачи по Теме 7.

---
+++ Theme06_Tasks/Theme06_Task01/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TextAnalyzer
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _textContent = "";
+        private bool _showDetails = false;
+
+        public string TextContent
+        {
+            get => _textContent;
+            set
+            {
+                if (_textContent != value)
+                {
+                    _textContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool ShowDetails
+        {
+            get => _showDetails;
+            set
+            {
+                if (_showDetails != value)
+                {
+                    _showDetails = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Theme06_Tasks/Theme06_Task01/MainWindow.xaml.cs
+++ Theme06_Tasks/Theme06_Task01/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TextAnalyzer;
 
 namespace Theme06_Task01
 {
@@ -16,7 +8,7 @@ namespace Theme06_Task01
         public MainWindow()
         {
             InitializeComponent();
-
+            DataContext = new MainViewModel();
         }
     }
 }
