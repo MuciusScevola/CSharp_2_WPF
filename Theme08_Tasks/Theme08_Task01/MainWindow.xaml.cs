046db50a04acb792230e51fd7e85ca70b63230f9
Ilya Par <ilya-par@yandex.ru>
2025-08-16 12:34:11 +0300

    Решение задачи.

--- Theme08_Tasks/Theme08_Task01/MainWindow.xaml.cs
+++ Theme08_Tasks/Theme08_Task01/MainWindow.xaml.cs
@@ -1,24 +1,60 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Theme08_Task01
+namespace ProductList
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public enum ProductCategory
+    {
+        Food,
+        Appliance
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        public ProductCategory Category { get; set; }
+    }
+
+    public class CategoryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ProductCategory category)
+            {
+                return category == ProductCategory.Appliance ? "Appliance" : "Food";
+            }
+            return "Food";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public partial class MainWindow : Window
     {
+        public List<Product> Products { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Пример данных
+            Products = new List<Product>
+            {
+                new Product { Name = "Яблоки", Price = 299.99m, ImagePath = "apple.jpg", Category = ProductCategory.Food },
+                new Product { Name = "Холодильник", Price = 59999.99m, ImagePath = "fridge.jpg", Category = ProductCategory.Appliance },
+                new Product { Name = "Бананы", Price = 199.49m, ImagePath = "banana.jpg", Category = ProductCategory.Food }
+            };
+
+            DataContext = this;
         }
     }
 }
