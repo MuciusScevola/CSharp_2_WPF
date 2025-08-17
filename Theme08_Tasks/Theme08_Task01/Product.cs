520068bd9e2ae1f938e05fd45d9810c2bc9e0881
Ilya Par <ilya-par@yandex.ru>
2025-08-17 23:05:41 +0300

    Решение задачи.

---
+++ Theme08_Tasks/Theme08_Task01/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme08_Task01
+{
+    public enum ProductCategory
+    {
+        Food,
+        Technics
+    }
+
+    public class Product
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public ProductCategory Category { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
---
+++ Theme08_Tasks/Theme08_Task01/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using WpfProductListApp.Models;
+
+namespace WpfProductListApp.ViewModels
+{
+    public class MainViewModel
+    {
+        // ObservableCollection автоматически уведомляет интерфейс об изменениях (добавление/удаление элементов)
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainViewModel()
+        {
+            // Инициализация и заполнение списка товаров
+            Products = new ObservableCollection<Product>
+            {
+                new Product
+                {
+                    Name = "Яблоки",
+                    Price = 299.99m,
+                    Category = ProductCategory.Food,
+                    // Путь к ресурсу изображения
+                    ImagePath = "/Images/apple.png"
+                },
+                new Product
+                {
+                    Name = "Холодильник",
+                    Price = 59999.99m,
+                    Category = ProductCategory.Appliances,
+                    ImagePath = "/Images/fridge.png"
+                },
+                new Product
+                {
+                    Name = "Бананы",
+                    Price = 199.49m,
+                    Category = ProductCategory.Food,
+                    ImagePath = "/Images/bananas.png"
+                }
+            };
+        }
+    }
+}
