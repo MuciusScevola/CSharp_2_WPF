37f522301e61602f94116ef3d11b99a16ce48741
Ilya Par <ilya-par@yandex.ru>
2025-08-23 12:06:34 +0300

    Решение задачи.

---
+++ Theme09_Tasks/Theme09_Task01/CustomCommands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme09_Task01
+{
+    internal class CustomCommands
+    {
+
+    }
+}
