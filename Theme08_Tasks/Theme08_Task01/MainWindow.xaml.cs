851d29a2ece2b876f600631b7ebbd0aa7821ac44
Ilya Par <ilya-par@yandex.ru>
2025-08-24 23:46:56 +0300

    Решение задачи по Теме 8.

--- Theme08_Tasks/Theme08_Task01/MainWindow.xaml.cs
+++ Theme08_Tasks/Theme08_Task01/MainWindow.xaml.cs
@@ -5,43 +5,45 @@ namespace Theme08_Task01
 {
     public partial class MainWindow : Window
     {
+        public List<Product> Products { get; set; } = new List<Product>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            List<Product> products = new List<Product>
+            Products.AddRange(new List<Product>
             {
                 new Product
                 {
                     Name = "Апельсины",
-                    Price = 299.99M,
-                    Category = ProductCategory.Food, 
-                    Image = "/Images/orange.png"
+                    Price = 129.99M,
+                    Category = ProductCategory.Food,
+                    ImagePath = "/Images/orange.png"
                 },
                 new Product
                 {
                     Name = "Стиральная машина",
-                    Price = 59999.99M,
+                    Price = 65799.99M,
                     Category = ProductCategory.Appliances,
-                    Image = "/Images/washing_machine.png"
+                    ImagePath = "/Images/washing_machine.png"
                 },
                 new Product
                 {
                     Name = "Виноград",
-                    Price = 199.49M,
+                    Price = 349.49M,
                     Category = ProductCategory.Food,
-                    Image = "/Images/grapes.png"
+                    ImagePath = "/Images/grapes.png"
                 },
                 new Product
                 {
                     Name = "Кофе-машина",
-                    Price = 199.49M,
+                    Price = 17999.00M,
                     Category = ProductCategory.Appliances,
-                    Image = "/Images/coffee_machine.png"
+                    ImagePath = "/Images/coffee_machine.png"
                 }
-            };
+            });
 
-            ProductsListBox.ItemsSource = products;
+            this.DataContext = this;
         }
     }
 }
--- Theme08_Tasks/Theme08_Task01/Product.cs
+++ Theme08_Tasks/Theme08_Task01/Product.cs
@@ -11,6 +11,6 @@
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public ProductCategory Category { get; set; }
-        public string? Image { get; set; }
+        public string? ImagePath { get; set; }
     }
 }
