169eaa9dd55fc2ca19549ac67937480d25f74f91
User <User@DESKTOP-N6DQC6G>
2025-08-27 20:36:44 +0300

    Решение задачи.

---
+++ Theme10_Tasks/Theme10_Task01/Models/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme10_Task01.Models
+{
+    public static class AuthModel
+    {
+        public const string ValidUsername = "admin";
+        public const string ValidPassword = "password";
+
+        public static bool Authenticate(string username, string password)
+        {
+            return username == ValidUsername && password == ValidPassword;
+        }
+    }
+}
---
+++ Theme10_Tasks/Theme10_Task01/ViewModels/IRelayCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Theme10_Task01.ViewModels
+{
+    internal interface IRelayCommand : ICommand
+    {
+        void Execute(object parameter);
+        bool CanExecute(object parameter);
+    }
+}
